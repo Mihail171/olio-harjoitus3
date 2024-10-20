4da6879aa2956558f0fef8820fbc175099519707
Mihail Lonstantinov <Konstantinovm1986@gmail.com>
2024-10-20 12:33:53 +0300

    lisätty uuden projektin Carluokka

---
+++ CarLuokka/car.h
@@ -0,0 +1,18 @@
+#ifndef CAR_H
+#define CAR_H
+#include<iostream>
+#include<string>>
+using namespace std;
+
+
+class Car
+{
+private:
+    string brand;
+    string model;
+    int yearModel;
+public:
+    Car();
+};
+
+#endif // CAR_H
--- H2a.cpp/main.cpp
+++ H2a.cpp/main.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
 
 using namespace std;
-
+class Person{
+private:
+    int age;
+    string name;
+public:
+    int getAge() const {
+        return age;
+    }
+    void setAge(int value){
+        age=value;
+    }
+    string getName() const {
+        return name;
+    }
+    void setName(string value){
+        name=value;
+    }
+};
 int main()
 {
-    cout << "Hello World!" << endl;
-    cout <<"hello boy"<<endl;
-    cout<<"ooooo";
-    cout<<"popitka n2";
-        cout<<"popitka n2";
-
+    cout << "class example" << endl;
+    Person objectPerson1;
+    objectPerson1.setAge(23);
+    objectPerson1.setName("mihail");
+    cout<<objectPerson1.getName()<<endl;
+    cout<<objectPerson1.getAge();
+    //pe.setAge(22);
+   // pe.setName("Mihail Konstantinov");
+    Person *objectPerson2;
+    objectPerson2=new Person();
 
     return 0;
 }
